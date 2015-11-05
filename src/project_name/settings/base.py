71f9b6e3c05b137244b063e283104d16fdfcf12e
shangliuyan <shangliuyan@gmail.com>
2015-11-05 13:45:12 +0800

    fix screct bug

--- src/project_name/settings/base.py
+++ src/project_name/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')vve&&*_t4vk8tkd@amssw1ixca=x*o8rmjzpa308**d%w(0-6'
+SECRET_KEY = r"{{ secret_key }}"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
